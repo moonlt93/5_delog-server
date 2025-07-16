66383f5d1694cab1c4f24540bfa82a9f9302dd6c
jinsu <moonlt932@gmail.com>
2025-07-16 23:44:02 +0900

    feat: batch bean 및 구성 컴포넌트 추가

---
+++ src/main/kotlin/com/delog/server/aggregate/statistics/infra/batch/config/BatchSchemaConfig.kt
@@ -0,0 +1,25 @@
+package com.delog.server.aggregate.statistics.infra.batch.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.jdbc.datasource.init.DataSourceInitializer
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator
+import javax.sql.DataSource
+
+@Configuration
+class BatchSchemaConfig {
+
+    @Bean
+    fun dataSourceInitializer(dataSource: DataSource): DataSourceInitializer {
+        val initializer = DataSourceInitializer()
+        initializer.setDataSource(dataSource)
+
+        val populator = ResourceDatabasePopulator()
+        populator.addScript(ClassPathResource("schema-batch.sql"))
+        initializer.setDatabasePopulator(populator)
+
+        return initializer
+
+    }
+}
