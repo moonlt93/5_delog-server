2b9a7df285cea76197e6400fe90f87e2be0c42f9
jinsu <moonlt932@gmail.com>
2025-07-16 23:54:49 +0900

    feat: 통계 상세 조회 API 추가

---
+++ src/main/kotlin/com/delog/server/aggregate/statistics/service/GetDeliveryOrderListService.kt
@@ -0,0 +1,33 @@
+package com.delog.server.aggregate.statistics.service
+
+import com.delog.server.aggregate.order.domain.entity.DeliveryOrderEntity
+import com.delog.server.aggregate.order.persistence.jpa.repository.DeliveryOrderRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class GetDeliveryOrderListService(
+    private val deliveryOrderRepository: DeliveryOrderRepository
+) {
+
+    fun getOrderListByUser(username: String, currentDate: LocalDate): List<DeliveryOrderEntity> {
+
+        val endDate = currentDate.minusDays(1).atTime(23, 59, 59)
+        val startDate = currentDate.minusDays(6).atStartOfDay()
+
+        return deliveryOrderRepository.findDataByWeekAtSunday(startDate, endDate, username);
+    }
+
+
+    fun getAllUsernames(currentDate: LocalDate): List<String> {
+
+        val endDate = currentDate.minusDays(1).atTime(23, 59, 59)
+        val startDate = currentDate.minusDays(6).atStartOfDay()
+
+        return deliveryOrderRepository
+            .findRangeUsername(startDate, endDate)
+            .map { it }
+    }
+
+}
