f27972b07788eed17da505672a5b4e2b78ed23af
jinsu <moonlt932@gmail.com>
2025-07-16 23:44:02 +0900

    feat: batch bean 및 구성 컴포넌트 추가

---
+++ src/main/kotlin/com/delog/server/aggregate/statistics/infra/batch/config/StatsBatchConfig.kt
@@ -0,0 +1,87 @@
+package com.delog.server.aggregate.statistics.infra.batch.config
+
+import com.delog.server.aggregate.statistics.domain.Stats
+import com.delog.server.aggregate.statistics.service.GetDeliveryOrderListService
+import com.delog.server.aggregate.statistics.service.SaveStaticsDataService
+import org.slf4j.LoggerFactory
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.support.ListItemReader
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+import java.time.LocalDate
+
+
+@Configuration(proxyBeanMethods = true)
+@EnableBatchProcessing
+class StatsBatchConfig(
+    private val jobRepository: JobRepository,
+    private val txManager: PlatformTransactionManager,
+    private val getDeliveryOrderListService: GetDeliveryOrderListService,
+    private val saveStaticsDataService: SaveStaticsDataService
+) {
+
+    @Bean
+    fun userListReader(): ItemReader<List<String>> {
+        val currentDate = LocalDate.now()
+        val allUsernames = getDeliveryOrderListService
+            .getAllUsernames(currentDate)
+            .distinct()
+        return ListItemReader(listOf(allUsernames))
+    }
+
+    @Bean
+    fun statsListProcessor(): ItemProcessor<List<String>, List<Stats>> = ItemProcessor { usernames ->
+        val results = mutableListOf<Stats>()
+        val currentDate = LocalDate.now()
+
+        usernames.forEach { username ->
+
+            val orders = getDeliveryOrderListService.getOrderListByUser(username, currentDate)
+
+            if (orders.isEmpty()) return@forEach
+
+            val stats = Stats
+                .createDomainFromEntity(orders, currentDate)
+                .apply { this.username = username }
+
+            results += stats
+        }
+        results
+    }
+
+    @Bean
+    fun statsListWriter(): ItemWriter<List<Stats>> = ItemWriter { chunks ->
+        val statsList: List<Stats> = chunks.first()
+
+        statsList.forEach { stats ->
+            saveStaticsDataService.saveStatisticsWeeklyData(stats)
+        }
+
+    }
+
+    @Bean
+    fun statsStep(): Step =
+        StepBuilder("statsStep", jobRepository)
+            .chunk<List<String>, List<Stats>>(1, txManager)
+            .reader(userListReader())
+            .processor(statsListProcessor())
+            .writer(statsListWriter())
+            .build()
+
+    @Bean
+    fun statsJob(): Job =
+        JobBuilder("statsJob", jobRepository)
+            .start(statsStep())
+            .build()
+
+}
+
---
+++ src/main/kotlin/com/delog/server/aggregate/statistics/infra/batch/config/StatsJobLauncher.kt
@@ -0,0 +1,39 @@
+package com.delog.server.aggregate.statistics.infra.batch.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import kotlin.jvm.java
+
+@Component
+class StatsJobLauncher(
+    private val jobLauncher: JobLauncher,
+    private val statsJob: Job,
+    private val jobRepository: JobRepository
+) {
+    private val logger = LoggerFactory.getLogger(JobLauncher::class.java)
+
+    @Scheduled(cron = "* */5 * * * *", zone = "Asia/Seoul")
+    fun runWeeklyStatsJob() {
+
+        val params = JobParametersBuilder()
+            .addLong("runAt", System.currentTimeMillis())
+            .toJobParameters()
+
+        try {
+
+            logger.info("Starting statsJob with parameters: {}", params)
+
+            val execution = jobLauncher.run(statsJob, params)
+
+        } catch (e: Exception) {
+
+            logger.error("statsJob failed", e)
+        }
+    }
+}
+
